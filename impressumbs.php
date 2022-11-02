48ee308ade99126edd5dc3ae09b76e378d3ec239
Yodirna <ridoy014@gmail.com>
2022-11-02 12:07:03 +0100

    test

---
+++ impressumbs.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-light mb-3">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">Hillton Hotel</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="registerbs.php">Registrierung</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="loginbs.php">Login</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="hilfebs.php">Hilfe</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="impressumbs.php">Impressum</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+    <h1 class="container text-center">Hillton Hotel</h1>
+    <div class="container text-center">
+        Hilltonplatz 1<br>
+        A-1010 Wien<br>
+        Tel.: +43 1 222 333<br>
+        E-Mail: <a href="mailto:support@hillton.com">support@hillton-hotel.com</a><br>
+        Firmenbuchnummer: 123456a<br>
+        Firmenbuchgericht: Gerichtstand Wien<br>
+        Aufsichtsbehörde: Magistrat Stadt Wien<br>
+        Mitgliedschaft: WKO, ÖHV<br>
+        UID-Nummer: ATU123345678<br>
+    </div>
+    <div class="container text-center mt-3">
+        <h3>Hotelverwaltung</h3>
+    </div>
+    <div class="container text-center">
+      <p><b>James Andre Hu (Geschäftsführer)</b></p>
+      <img src="img/james.jpg" alt="James" width="400">
+      <p><b>Ridoy Rahman (Geschäftsführer)</b></p>
+      <img src="img/ridoy.jpg" alt="Ridoy" width="400">
+  </p>
+  </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
