4e21dec6a05c38fa2531e87cee4a0d38e7459741
weiminghu31 <weiminghu31@gmail.com>
2022-11-13 17:24:54 +0100

    yes big changes

--- index.php
+++ index.php
@@ -4,35 +4,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hillton Startseite</title>
+    <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="header">
+    <nav class="navbar navbar-dark navbar-expand-lg">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.php">Hillton Hotel</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="#">HILLTON</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavButton" aria-controls="navbarNavButton" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="registerbs.php">Registrierung</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="loginbs.php">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="hilfebs.php">Hilfe</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="impressumbs.php">Impressum</a>
-              </li>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavButton">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="#">NEWS</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">PRICING</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">BOOKING</a>
+                </li>
+                <button type="button" class="btn btn-outline-light">LOGIN</button>
             </ul>
-          </div>
+            </div>
         </div>
-      </nav>
-      <img src="img/hillton.jpg" class="img-fluid " alt="hi">
+        </nav>
+
+        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img src="img/lobby.jpg" class="d-block w-100"> 
+                </div>
+                <div class="carousel-item">
+                <img src="img/drink.jpg" class="d-block w-100">
+                </div>
+                <div class="carousel-item">
+                <img src="img/smallroom.jpg" class="d-block w-100">
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+            </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
