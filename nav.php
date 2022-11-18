3bbe064059ac3ff882638e39265d98daa6600c45
Yodirna <ridoy014@gmail.com>
2022-11-18 19:22:24 +0100

    made home n nav file

---
+++ nav.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-light navbar-expand-lg bg-light">
+        <div class="container-fluid">
+
+            <a class="navbar-brand" href="#">
+                HILLTON
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavButton" aria-controls="navbarNavButton" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon">
+                </span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNavButton">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">NEWS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">PRICING</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">BOOKING</a>
+                    </li>
+                        <a href="loginbs.php" class="btn btn-outline-light">LOGIN</a>
+                </ul>
+            </div>
+        </div>
+</nav>
