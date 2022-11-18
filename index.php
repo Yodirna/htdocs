c8ebef1afda183bb2aa3b8175447c7a3aca08688
Yodirna <ridoy014@gmail.com>
2022-11-18 19:19:53 +0100

    indexphp dynamic nav

--- index.php
+++ index.php
@@ -6,71 +6,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <style>
+      <?php include "style.css" ?>
+    </style>
 </head>
 <body>
-    <div class="header">
-    <nav class="navbar navbar-dark navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">HILLTON</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavButton" aria-controls="navbarNavButton" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavButton">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                <a class="nav-link" href="#">NEWS</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">PRICING</a>
-                </li>
-                <li class="nav-item">
-                <a class="nav-link" href="#">BOOKING</a>
-                </li>
-                <a href="loginbs.php" class="btn btn-outline-light">LOGIN</a>
 
-            </ul>
-            </div>
+    <?php
+        // fetch current "site" (or set to "home" if not defined)        
+        $site = $_GET["site"] ?? "home";
+
+        // for security reasone:  check if $site is in a list of available sites
+        $sites = [ "home", "loginbs", "registerbs", "impressumbs", "hilfebs"];
+        if (!in_array($site, $sites)) {
+            $error = "Seite nicht gefunden - " . $site;
+            $site = "error";    
+        }
+
+        // render nav bar
+        include_once "nav.php";
+    ?>
+
+    <?php
+
+        // render site here ...
+        include_once $site . ".php";
+    ?>
+
+    <footer class="bg-light text-center text-lg-start">
+        <div>
+            <li>
+                <a class="link" href="hilfebs.php">HILFE</a>
+            </li>
+            <li>
+                <a class="link" href="impressumbs.php">IMPRESSUM</a>
+            </li>
         </div>
-        </nav>
+    </footer>
 
-        <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img src="img/lobby.jpg" class="d-block w-100"> 
-                </div>
-                <div class="carousel-item">
-                <img src="img/drink.jpg" class="d-block w-100">
-                </div>
-                <div class="carousel-item">
-                <img src="img/smallroom.jpg" class="d-block w-100">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-            </div>
-    </div>
-    <div class="stuff_inbetween">
-        <h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, laboriosam? Earum alias voluptatibus minus consequatur ea iure amet aspernatur cum, nemo deserunt ullam nulla nostrum quia minima atque quisquam. Quasi?
-            </p>
-        </h1>
-    </div>
-    <div>
-        <li>
-            <a class="link" href="hilfebs.php">HILFE</a>
-        </li>
-        <li>
-            <a class="link" href="impressumbs.php">IMPRESSUM</a>
-        </li>
-    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 </body>
 
