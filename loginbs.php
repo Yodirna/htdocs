16b2a0f6202040cd31ea35d16b484a3f531cf14d
Yodirna <ridoy014@gmail.com>
2022-11-18 19:41:52 +0100

    loginbs and bgstyle

--- loginbs.php
+++ loginbs.php
@@ -5,10 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <style>
+      <?php include "bgstyle.css" ?>
+    </style>
+
 </head>
+
 <body>
-    <nav class="navbar navbar-expand-lg bg-light mb-3">
+    <nav class="navbar navbar-expand-lg navbar-dark mb-3" style="background-color: #B85042;">
         <div class="container-fluid">
           <a class="navbar-brand" href="index.php">Hillton Hotel</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
