9a818120f43c126ed706520fc79f0297f898a2c2
Yodirna <ridoy014@gmail.com>
2022-11-18 19:51:58 +0100

    asdasdas

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Homepage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <style>
-      <?php include "style.css" ?>
+      <?php include "bgstyle.css" ?>
     </style>
 </head>
 <body>
@@ -33,7 +33,7 @@
         include_once $site . ".php";
     ?>
 
-    <footer class="footer bg-light text-center text-lg-start fixed-bottom">
+    <footer class="footer text-center text-lg-start fixed-bottom">
         <div>
             <li>
                 <a class="link" href="hilfebs.php">HILFE</a>
