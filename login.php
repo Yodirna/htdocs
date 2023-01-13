560167f33a0e667fbcb79048a2e1c97adcaa2d33
weiminghu31 <weiminghu31@gmail.com>
2023-01-13 16:38:50 +0100

    kb mehr

--- login.php
+++ login.php
@@ -25,7 +25,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $_SESSION["user_id"] = $row["id"];
 
                     $_GET["site"] = "home";
-                    include"index.php";
+                    include "index.php";
                     exit();
                 }else{
                     $error = "Password or Email dont match";
