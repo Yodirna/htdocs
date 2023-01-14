6a9a439fd44d692694083e83d7a9305f4757a4a0
Yodirna <ridoy014@gmail.com>
2023-01-14 19:09:16 +0100

    resresrse

--- zreservieren.php
+++ zreservieren.php
@@ -1,16 +1,20 @@
-<p class="container text-center mt-5">
-    Neue Zimmerreservierung</p>
+<div class="container text-center mt-5">
+    <p>
+    Zimmer buchen</p>
+</div>
+
 
 <form class="form-group" action="" method="POST">
+    <div class=" text-center">
+        <div class=" container input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Anreisedatum</span>
+            <input type="date" value="2023-01-01" class="form-control" id="von" name="resvon">
+        </div>
 
-    <div class="container input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">Anreisedatum</span>
-        <input type="date" value="2023-01-01" class="form-control" id="von" name="resvon">
-    </div>
-
-    <div class="container input-group mb-3">
-        <span class="input-group-text" id="basic-addon1">Abreisedatum</span>
-        <input type="date" value="2023-01-01" class="form-control" id="bis" name="resbis">
+        <div class=" container input-group mb-3">
+            <span class="input-group-text" id="basic-addon1">Abreisedatum</span>
+            <input type="date" value="2023-01-01" class="form-control" id="bis" name="resbis">
+        </div>
     </div>
 
     <div class ="container">
@@ -29,12 +33,16 @@
         </div>
     </div>
 
-    <div class=" container mb-3 form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <div class="container">
+    <div class="container btn-group mb-3" role="group" aria-label="Basic radio toggle button group">
+            <input type="radio" class="btn-check" name="haustier" id="haustier" autocomplete="off" checked>
+            <label class="btn btn-outline-primary" for="haustier">Anreise mit Haustier</label>
+            <input type="radio" class="btn-check" name="haustier" id="khaustier" autocomplete="off">
+            <label class="btn btn-outline-primary" for="khaustier">Anreise ohne Haustier</label>
+        </div>
     </div>
 
-    <div class="container"> 
-        <button type="submit" class="btn btn-primary">Submit</button>
+    <div class="container text-center"> 
+        <button type="submit" class="btn btn-lg btn-primary">Submit</button>
     </div>
 </form>
